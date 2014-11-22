d65ff7a640dd19d773b7d63e60e7abbec104adf8
kien nguyen <tgsoon2002@hotmail.com>
2014-11-21 17:33:29 -0800

    Student Database
    Start new program, show database, working on class of student

---
+++ DatabaseView/DatabaseView/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabaseView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataGrid localData = new DataGrid();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Student newData = new Student();
+            DataGrid.ItemsSource = newData.GetEmpList();    
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = null;
+        }
+    }
+}
---
+++ DatabaseView/DatabaseView/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseView
+{
+    class Student
+    {
+        enum status { current, pending, graduate, cancel };
+        enum visaStatus { F1, M1, other };
+        int id;
+        string _FName;
+        string _LName;
+        string _MName;
+        status _status;
+        bool _FinaAid;
+        visaStatus _visaStats;
+        bool _fullTime;
+        DateTime _StartDate;
+        DateTime _ScheduleDate;
+        DateTime _EndDate;
+        DateTime _DOB;
+        string _Note;
+        string _Address;
+        string _City;
+        string _State;
+        int _Zip;
+        int _CellPhone;
+        int _HomePhone;
+
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string FirstName
+        {
+            get { return _FName; }
+            set { _FName = value; }
+        }
+        public string LastName
+        {
+            get { return _LName; }
+            set { _LName = value; }
+        }
+        public status Status
+        {
+            get { return _status; }
+            set { _status = value; } 
+        }
+
+
+
+        public List<Student> GetEmpList()
+        {
+            List<Student> temp = new List<Student>();
+            temp.Add(new Student() { id = 1, FirstName = "Kien", LastName = "Nguyen" });            
+            return temp;
+        }
+    }
+}
